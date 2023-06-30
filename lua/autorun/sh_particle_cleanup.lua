b564aa310983d91a46b9ec56a9563a558fcc0344
Zaurzo <101999193+Zaurzo@users.noreply.github.com>
2023-06-30 14:37:13 -0400

    Fixes and improvements
    - Don't localize the destroy functions, but instead index them from the particle object
    - Remove CLuaEffect(s) created from util.Effect
    - Don't localize so much
    - Use game.GetWorld() instead of Entity(0)

--- lua/autorun/sh_particle_cleanup.lua
+++ lua/autorun/sh_particle_cleanup.lua
@@ -1,23 +1,13 @@
 if CLIENT then
-    local _R = debug.getregistry()
-
     local particleObjectVault = {}
-
-    local StopAndDestroyParticles = _R.Entity.StopAndDestroyParticles
-    local ents_GetAll = ents.GetAll
-
-    local isentity = isentity
-    local ipairs = ipairs
-    local pairs = pairs
-    local ents = ents
 
     do
         -- StealParticleObject
         -- Detour a particle creation function and save it's returned object to particleObjectVault
         
-        -- @arg funcDestroyParticle
-        -- The function to call on the particle object to completely destroy it
-        local function StealParticleObject(meta, funcName, funcDestroyParticle)
+        -- @arg funcDestroyParticleName
+        -- The name of the function to call on the particle object to completely destroy it
+        local function StealParticleObject(meta, funcName, funcDestroyParticleName)
             local createParticle = meta[funcName]
 
             if createParticle then
@@ -26,7 +16,7 @@ if CLIENT then
 
                     if particleObj then
                         if not particleObj.IsValid or particleObj:IsValid() then
-                            particleObjectVault[particleObj] = funcDestroyParticle
+                            particleObjectVault[particleObj] = funcDestroyParticleName
                         end
                     end
 
@@ -35,21 +25,34 @@ if CLIENT then
             end
         end
 
-        StealParticleObject(_R.CLuaEmitter, 'Add', _R.CLuaParticle.SetDieTime)
-        StealParticleObject(_G, 'CreateParticleSystemNoEntity', _R.CNewParticleEffect.StopEmissionAndDestroyImmediately)
+        local CLuaEmitter = debug.getregistry().CLuaEmitter
+
+        StealParticleObject(CLuaEmitter, 'Add', 'SetDieTime')
+        StealParticleObject(_G, 'CreateParticleSystemNoEntity', 'StopEmissionAndDestroyImmediately')
     end
+
+    local ents = ents
+    local isentity = isentity
+    local ipairs = ipairs
+    local pairs = pairs
 
     local function CleanupParticles(cleanupType)
         if cleanupType and cleanupType ~= 'particles' then return end
         
-        for k, ent in ipairs(ents_GetAll()) do
-            if isentity(ent) then
-                StopAndDestroyParticles(ent)
+        for k, ent in ipairs(ents.GetAll()) do
+            if isentity(ent) and ent ~= NULL then
+                ent:StopAndDestroyParticles()
+
+                if ent:GetClass() == 'class CLuaEffect' then
+                    ent:Remove()
+                end
             end
         end
 
-        for particleObj, destroy in pairs(particleObjectVault) do
-            if not particleObj.IsValid or particleObj:IsValid() then
+        for particleObj, funcDestroyParticleName in pairs(particleObjectVault) do
+            local destroy = particleObj[funcDestroyParticleName]
+
+            if destroy then
                 destroy(particleObj, 0)
             end
 
@@ -60,22 +63,17 @@ if CLIENT then
     language.Add('Cleanup_particles', 'Particles')
 
     hook.Add('OnCleanup', 'particle_cleanup', CleanupParticles)
-    hook.Add('PostCleanupMap', 'particle_cleanup', CleanupParticles)
+    hook.Add('PreCleanupMap', 'particle_cleanup', CleanupParticles)
 end
 
 local ParticleEffect = ParticleEffect
-local Entity = Entity
-local World
+local game_GetWorld = game.GetWorld
 
--- If there wasn't any parent provided, force it to be the World entity instead.
+-- If there wasn't a parent provided, force it to be the World entity instead.
 -- This is so I can remove these particles with Entity:StopAndDestroyParticles()
 function _G.ParticleEffect(particleName, position, angles, parent, ...)
-    if not isentity(parent) then
-        if not World then
-            World = Entity(0)
-        end
-
-        parent = World
+    if not parent then
+        parent = game_GetWorld()
     end
 
     return ParticleEffect(particleName, position, angles, parent, ...)
